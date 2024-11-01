908852a808ce58867aaffc565330b17d4e01ab99
TheWaspsAreHere <719045@student.dusd.net>
2024-11-01 10:00:08 -0700

    New Interactables
    Made new interactables in the game

--- 2DBeginnerAdventureP2Morales/Assets/Scripts/RubyController.cs
+++ 2DBeginnerAdventureP2Morales/Assets/Scripts/RubyController.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class RubyController : MonoBehaviour
 {
+    public float speed = 3.0f;
+    public int maxHealth = 5;
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -12,25 +18,28 @@ public class NewBehaviourScript : MonoBehaviour
     void Start()
     {
        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        currentHealth = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
-
-       
     }
     private void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime;
-
-        transform.position = position;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime; ;
 
         rigidbody2d.MovePosition(position);
+    }
+   public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
