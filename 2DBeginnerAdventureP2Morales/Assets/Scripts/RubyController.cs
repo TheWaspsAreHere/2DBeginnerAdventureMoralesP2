83f325cf119741e5eb3ababb9dbe8c7c5a035084
TheWaspsAreHere <719045@student.dusd.net>
2024-11-14 10:00:05 -0800

    Animation
    Finish the animation coding for Ruby

--- 2DBeginnerAdventureP2Morales/Assets/Scripts/RubyController.cs
+++ 2DBeginnerAdventureP2Morales/Assets/Scripts/RubyController.cs
@@ -7,7 +7,7 @@ public class RubyController : MonoBehaviour
     public float speed = 3.0f;
 
     public int maxHealth = 5;
-    public float timeInvincible = 2;
+    public float timeInvincible = 2.0f;
     public int health { get { return currentHealth; } }
     int currentHealth;
 
@@ -16,12 +16,16 @@ public class RubyController : MonoBehaviour
     float horizontal;
     float vertical;
 
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+
     // Start is called before the first frame update
     void Start()
     {
        rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
         currentHealth = 1;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -30,7 +34,19 @@ public class RubyController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+        }
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
+        animator.SetFloat("Speed", move.magnitude);
     }
+
+
     private void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
@@ -41,6 +57,17 @@ public class RubyController : MonoBehaviour
     }
    public void ChangeHealth(int amount)
     {
+        if(amount < 0)
+        {
+            animator.SetTrigger("Hit");
+
+                if(isInvicible)
+            {
+                return;
+            }
+            isInvincible = true;
+            invincibleTimer = timeInvincible;
+        }
         currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
