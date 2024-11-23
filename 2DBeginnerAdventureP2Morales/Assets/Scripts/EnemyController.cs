af1c6a252256bc526c448a11fe5f7f696c24bde9
TheWaspsAreHere <719045@student.dusd.net>
2024-11-22 18:27:00 -0800

    EnemyControllerName
    The name of the public class was off from the reference so I changed the public class name to EnemyController

--- 2DBeginnerAdventureP2Morales/Assets/Scripts/EnemyController.cs
+++ 2DBeginnerAdventureP2Morales/Assets/Scripts/EnemyController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
     public float speed = 3.0f;
     public bool vertical;
